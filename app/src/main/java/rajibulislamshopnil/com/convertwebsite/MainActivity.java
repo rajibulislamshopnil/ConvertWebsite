5cab40c99f2705f97b2b95e733ec7e128a6c15e8
Rajibul Islam Shopnil <Rajibul Islam Shopnil>
2017-02-12 13:26:15 +0600

    Rajibul Islam Shpnil

---
+++ app/src/main/java/rajibulislamshopnil/com/convertwebsite/MainActivity.java
@@ -0,0 +1,31 @@
+package rajibulislamshopnil.com.convertwebsite;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = (WebView)findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("http://shopnilsopno.blogspot.com/");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()){
+            webView.goBack();
+        }
+        else {
+            super.onBackPressed();
+        }
+
+    }
+}
